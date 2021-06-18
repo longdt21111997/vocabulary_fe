6e0cc96a3f7e60b2ae3141d2e9b4f4056deaf0d7
longdt21111997 <63586598+longdt21111997@users.noreply.github.com>
2021-06-18 20:07:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+app.use(requireHTTPS);
 app.use(express.static('./dist/vocabulary-fe'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: './dist/vocabulary-fe'}),
+  res.sendFile('index.html', {root: 'dist/vocabulary-fe/'}),
 );
 
+app.listen(3000);
 app.listen(process.env.PORT || 8080);
