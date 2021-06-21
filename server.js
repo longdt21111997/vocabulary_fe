c02a191eee4cf4bd6f6df58821dd155aafc4d281
longdt21111997 <63586598+longdt21111997@users.noreply.github.com>
2021-06-21 09:31:43 +0700

    bug

--- server.js
+++ server.js
@@ -1,41 +1,16 @@
-// function requireHTTPS(req, res, next) {
-//   // The 'x-forwarded-proto' check is for Heroku
-//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-//     return res.redirect('https://' + req.get('host') + req.url);
-//   }
-//   next();
-// }
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+const app = express();
 
-// const app = express();
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-// app.use(requireHTTPS);
-// app.use(express.static('./dist/vocabulary-fe'));
+app.get('/*', function(req,res) {
 
-// app.get('/*', (req, res) =>
-//   res.sendFile('index.html', {root: 'dist/vocabulary-fe/'}),
-// );
+  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
 
-// app.listen(3000);
-// app.listen(process.env.PORT || 8080);
-
-    //Install express server
-    const express = require('express');
-    const path = require('path');
-
-    const app = express();
-
-    // Serve only the static files form the dist directory
-    app.use(express.static('./dist/vocabulary-fe'));
-
-    app.get('/*', function(req,res) {
-        
-    res.sendFile(path.join('./dist/vocabulary-fe/index.html'));
-    });
-
-    // Start the app by listening on the default Heroku port
-    app.listen(process.env.PORT || 8080);
-
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
